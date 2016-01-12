584ee607b33c286ebaf113cc2323b4a835c1f0c2
marshalTre <mike_kids@hotmail.com>
2016-01-12 17:46:30 -0600

    Actualizado

---
+++ control/datos_conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author www.intercambiosvirtuales.org
+ * @copyright 2015
+ */
+
+  $servidor = "localhost";
+  $nombreDeUsuario = "root";
+  $contrasena = "rocasado78";
+  $baseDeDatos = "rocdf";
+
+?>
